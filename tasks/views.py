31474d67e501a67ca869633bc6c46330bedf37c3
samyog777 <samyogkoirala18@gmail.com>
2024-02-16 14:59:10 +0545

    Second Day

--- tasks/views.py
+++ tasks/views.py
@@ -1,13 +1,40 @@
+from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-tasks = ["foo", "bar", "baz"]
+#It is global list so any body can see lists
+# tasks = []
 
+
+class NewTask(forms.Form):
+    task = forms.CharField(label="New Task")
+    # priority = forms.IntegerField(label="priority", min_value=1, max_value=5)
 # Create your views here.
 
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+
     return render(request, "tasks/index.html", {
-        "tasks": tasks
+        # "tasks": tasks # for global list
+        "tasks": request.session["tasks"]
     })
 
 def add(request):
-    return render(request, "tasks/add.html")
+    if request.method == "POST":
+        form = NewTask(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            # tasks.append(task) #if list is global
+            request.session["tasks"] += [task]
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return (request, "tasks/add.html", {
+                "form": form
+            })
+        
+
+    return render(request, "tasks/add.html", {
+        "form": NewTask()
+    })
