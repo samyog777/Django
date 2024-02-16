a3f0c9c7523c5a072e82f60f406127c570910a14
samyog777 <samyogkoirala18@gmail.com>
2024-02-16 13:08:11 +0545

    Second Day

--- hello/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views #import the view file form . (current directory)
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("<str:name>", views.greet, name="greet"),
-    path("samyog", views.samyog, name="samyog")
-]
--- newyear/views.py
+++ newyear/views.py
@@ -3,9 +3,10 @@ import datetime
 from django.shortcuts import render
 
 # Create your views here.
-now = datetime.datetime.now()
 
 def index(request):
+    now = datetime.datetime.now()
     return render(request, "newyear/index.html", {
         "newyear": now.month == 1 and now.day == 1 
+        # "newyear": True
     })
